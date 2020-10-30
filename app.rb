08fa82f2585abf0120c4d6a7dcfe0d05a3b61110
Alie Stone <helloaliestone@gmail.com>
2020-10-30 13:39:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,13 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+   get '/:newteam' do
+     erb params[:newteam].to_sym
+   end
+
+   post '/:team' do
+    @results =  erb params[:newteam]
+    @results
+   end
 
 end
